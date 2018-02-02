38382d903b4aa7c17e8c05b787c2ab121fe41d22
owen-linton <owenlinton1@gmail.com>
2018-02-02 13:47:34 -0500

    First commit changes

---
+++ app/src/main/java/com/group18/cs446/spacequest/GamePlayActivity.java
@@ -0,0 +1,13 @@
+package com.group18.cs446.spacequest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GamePlayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_play_activity);
+    }
+}
