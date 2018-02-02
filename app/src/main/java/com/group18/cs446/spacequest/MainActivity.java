b098fad631e6ca69bed28137f2d2827df7419c86
owen-linton <owenlinton1@gmail.com>
2018-02-02 16:02:29 -0500

    Made button change views

--- app/src/main/java/com/group18/cs446/spacequest/MainActivity.java
+++ app/src/main/java/com/group18/cs446/spacequest/MainActivity.java
@@ -1,13 +1,26 @@
 package com.group18.cs446.spacequest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button game_start_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game_start_button = (Button) findViewById(R.id.game_start_button);
+        game_start_button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, GamePlayActivity.class));
     }
 }
