1b0a81bf2267eb9c1ca68f6d4d05a7231b5334bd
Johan Sjöberg <johasjob@student.chalmers.se>
2018-03-05 14:55:29 -0500

    Add initial layout draft

--- app/src/main/java/com/group18/cs446/spacequest/MainActivity.java
+++ app/src/main/java/com/group18/cs446/spacequest/MainActivity.java
@@ -8,23 +8,24 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button game_start_button;
+    //Button game_start_button;
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        System.out.println("MAIN RESUMING");
-    }
+    //@Override
+    //public void onResume() {
+    //    super.onResume();
+    //    System.out.println("MAIN RESUMING");
+    //}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_main);
+        setContentView(R.layout.shop_layout);
 
-        game_start_button = findViewById(R.id.game_start_button);
-        game_start_button.setOnClickListener(this);
-        View root = findViewById(android.R.id.content);
-        root.setSystemUiVisibility(Constants.BASE_UI_VISIBILITY);
+        //game_start_button = findViewById(R.id.game_start_button);
+        //game_start_button.setOnClickListener(this);
+        //View root = findViewById(android.R.id.content);
+        //root.setSystemUiVisibility(Constants.BASE_UI_VISIBILITY);
     }
 
     @Override
---
+++ app/src/main/java/com/group18/cs446/spacequest/selected_items.java
@@ -0,0 +1,19 @@
+package com.group18.cs446.spacequest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class selected_items extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_selected_items, container, false);
+    }
+
+}
