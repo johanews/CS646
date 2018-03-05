01f31b76df61e773eb2616f14730ee6363a5dafe
Johan Sjöberg <johasjob@student.chalmers.se>
2018-03-05 15:49:35 -0500

    Add ShopActivity

--- app/src/main/java/com/group18/cs446/spacequest/MainActivity.java
+++ app/src/main/java/com/group18/cs446/spacequest/MainActivity.java
@@ -6,30 +6,42 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity {
 
-    //Button game_start_button;
+    Button game_start_button;
+    Button shop_button;
 
-    //@Override
-    //public void onResume() {
-    //    super.onResume();
-    //    System.out.println("MAIN RESUMING");
-    //}
+    @Override
+    public void onResume() {
+        super.onResume();
+        System.out.println("MAIN RESUMING");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // setContentView(R.layout.activity_main);
-        setContentView(R.layout.shop_layout);
+        setContentView(R.layout.activity_main);
 
-        //game_start_button = findViewById(R.id.game_start_button);
-        //game_start_button.setOnClickListener(this);
-        //View root = findViewById(android.R.id.content);
-        //root.setSystemUiVisibility(Constants.BASE_UI_VISIBILITY);
+        game_start_button = findViewById(R.id.game_start_button);
+        game_start_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, GamePlayActivity.class));
+             }
+        });
+
+        shop_button = findViewById(R.id.shop_button);
+        shop_button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ShopActivity.class));
+            }
+        });
+
+        View root = findViewById(android.R.id.content);
+        root.setSystemUiVisibility(Constants.BASE_UI_VISIBILITY);
     }
 
-    @Override
-    public void onClick(View v) {
-        startActivity(new Intent(this, GamePlayActivity.class));
-    }
 }
