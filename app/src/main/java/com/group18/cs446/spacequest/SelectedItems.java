c1330f20e07e8290a3466cf540ac0e2a305ff652
Johan Sjöberg <johasjob@student.chalmers.se>
2018-03-07 20:02:32 -0500

    Add layout changes

---
+++ app/src/main/java/com/group18/cs446/spacequest/SelectedItems.java
@@ -0,0 +1,19 @@
+package com.group18.cs446.spacequest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SelectedItems extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.selected_items, container, false);
+    }
+
+}
