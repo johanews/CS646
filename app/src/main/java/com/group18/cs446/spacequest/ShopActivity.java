182800a049d17d672240035bfb238e005feadbd2
Johan Sjöberg <johasjob@student.chalmers.se>
2018-03-16 16:22:14 -0400

    Finish the rhs of the shop

--- app/src/main/java/com/group18/cs446/spacequest/ShopActivity.java
+++ app/src/main/java/com/group18/cs446/spacequest/ShopActivity.java
@@ -1,19 +1,14 @@
 package com.group18.cs446.spacequest;
 
-import android.app.ActionBar;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.group18.cs446.spacequest.game.objects.player.ComponentFactory;
 import com.group18.cs446.spacequest.game.objects.player.Engine;
-import com.group18.cs446.spacequest.game.objects.player.Hull;
-import com.group18.cs446.spacequest.game.objects.player.Player;
 import com.group18.cs446.spacequest.game.objects.player.PlayerInfo;
-import com.group18.cs446.spacequest.game.objects.player.Shield;
 import com.group18.cs446.spacequest.game.objects.player.Weapon;
 
 public class ShopActivity extends AppCompatActivity implements View.OnClickListener {
@@ -22,6 +17,9 @@ public class ShopActivity extends AppCompatActivity implements View.OnClickListe
     PlayerInfo playerInfo;
 
     Button play_button;
+
+    ComponentFactory factory;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +30,8 @@ public class ShopActivity extends AppCompatActivity implements View.OnClickListe
         View root = findViewById(android.R.id.content);
         root.setSystemUiVisibility(Constants.BASE_UI_VISIBILITY);
 
+        factory = new ComponentFactory(getBaseContext());
+
         selectedItems = (SelectedItems) getSupportFragmentManager().
                         findFragmentById(R.id.selected_items_fragment);
 
@@ -39,7 +39,7 @@ public class ShopActivity extends AppCompatActivity implements View.OnClickListe
         play_button.setOnClickListener(this);
 
         playerInfo = (PlayerInfo) getIntent().getSerializableExtra("PlayerInfo");
-        System.out.println("PLAYER INFO IN SHOP:" + playerInfo.getCurrentSector() + ": $" + playerInfo.getMoney());
+
         getWeapon();
         getEngine();
         getShield();
@@ -48,38 +48,38 @@ public class ShopActivity extends AppCompatActivity implements View.OnClickListe
 
     public void getWeapon() {
 
-        // int type = player.getWeapon();
-        // Weapon weapon = EquipmentFactory().getWeapon(type);
+        int type = playerInfo.getWeapon();
+        Weapon weapon = (Weapon) factory.getShipComponent(type);
 
-        // selectedItems.getWeaponFragment().setImage(weapon.getImage());
-        // selectedItems.getWeaponFragment().setTitle(weapon.getName());
+        selectedItems.getWeaponFragment().setImage(weapon.getBitmap());
+        selectedItems.getWeaponFragment().setTitle(weapon.getName());
     }
 
     public void getEngine() {
 
-        // int type = player.getWeapon();
-        // Weapon weapon = EquipmentFactory().getWeapon(type);
+        int type = playerInfo.getEngine();
+        Engine engine = (Engine) factory.getShipComponent(type);
 
-        // selectedItems.getWeaponFragment().setImage(weapon.getImage());
-        // selectedItems.getWeaponFragment().setTitle(weapon.getName());
+        selectedItems.getEngineFragment().setImage(engine.getBitmap());
+        selectedItems.getEngineFragment().setTitle(engine.getName());
     }
 
     public void getShield() {
 
-        // int type = player.getWeapon();
-        // Weapon weapon = EquipmentFactory().getWeapon(type);
+        int type = playerInfo.getWeapon();
+        Weapon shield = (Weapon) factory.getShipComponent(type);
 
-        // selectedItems.getWeaponFragment().setImage(weapon.getImage());
-        // selectedItems.getWeaponFragment().setTitle(weapon.getName());
+        selectedItems.getShieldFragment().setImage(shield.getBitmap());
+        selectedItems.getShieldFragment().setTitle(shield.getName());
     }
 
     public void getHull() {
 
-        // int type = player.getWeapon();
-        // Weapon weapon = EquipmentFactory().getWeapon(type);
+        int type = playerInfo.getWeapon();
+        Weapon hull = (Weapon) factory.getShipComponent(type);
 
-        // selectedItems.getWeaponFragment().setImage(weapon.getImage());
-        // selectedItems.getWeaponFragment().setTitle(weapon.getName());
+        selectedItems.getHullFragment().setImage(hull.getBitmap());
+        selectedItems.getHullFragment().setTitle(hull.getName());
     }
 
     @Override
