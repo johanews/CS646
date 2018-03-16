0379bea9075b0c812637866fd607a59aa9226227
Vegard Seim Karstang <vskarsta@edu.uwaterloo.ca>
2018-03-16 15:55:08 -0400

    Started Facebook integration.

---
+++ app/src/main/java/com/group18/cs446/spacequest/social/FacebookActivity.java
@@ -0,0 +1,17 @@
+package com.group18.cs446.spacequest.social;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.group18.cs446.spacequest.R;
+
+
+public class FacebookActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.id.);
+    }
+}
